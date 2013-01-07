3cc17b9270289400102e1756d366ac8cc7691f69
Todd Wolfson <todd@twolfson.com>
2013-01-06 20:57:37 -0800

    Adding some description to repo

---
+++ grunt.js
@@ -0,0 +1,42 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:crossbones.jquery.json>',
+    // qunit: {
+    //   files: ['test/**/*.html']
+    // },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      // tasks: 'lint qunit'
+      tasks: 'lint'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  // grunt.registerTask('default', 'lint qunit');
+  grunt.registerTask('default', 'lint');
+};
